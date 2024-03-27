febde7ab1c605b173991167f4ed5dde0e083afd7
Dan Whitacre <whitacre.74@osu.edu>
2024-03-26 21:33:09 -0400

    remove apps that dont work for tmnext, and some others comment out for now

--- trackmania/apps.py
+++ trackmania/apps.py
@@ -4,30 +4,14 @@ APPS = {
     'pyplanet.apps.contrib.jukebox',
     'pyplanet.apps.contrib.karma',
     'pyplanet.apps.contrib.local_records',
-    'pyplanet.apps.contrib.dedimania',
     'pyplanet.apps.contrib.players',
-    'pyplanet.apps.contrib.info',
+    # 'pyplanet.apps.contrib.info',
     'pyplanet.apps.contrib.mx',
-    'pyplanet.apps.contrib.transactions',
-
-    # New since 0.4.0:
-    'pyplanet.apps.contrib.sector_times',
-    'pyplanet.apps.contrib.dynamic_points',
-
-    # New since 0.5.0:
-    'pyplanet.apps.contrib.clock',
-    'pyplanet.apps.contrib.best_cps',
+    # 'pyplanet.apps.contrib.clock',
     'pyplanet.apps.contrib.voting',
-
-    # New since 0.6.0:
-    'pyplanet.apps.contrib.queue',
-    'pyplanet.apps.contrib.ads',
-    'pyplanet.apps.contrib.music_server',
-
-    # New since 0.8.0:
+    # 'pyplanet.apps.contrib.ads',
+    # 'pyplanet.apps.contrib.music_server',
     'pyplanet.apps.contrib.funcmd',
-
-    # New since 0.10.0:
     'pyplanet.apps.contrib.rankings',
   ],
 }
